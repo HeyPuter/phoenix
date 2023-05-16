c5c575ed5531ebbe2dbb34928ec06d206ef01cb5
KernelDeimos <eric@centos1.rack1.local>
2023-05-15 21:35:39 -0400

    Add mv command

--- src/puter-shell/coreutils/__exports__.js
+++ src/puter-shell/coreutils/__exports__.js
@@ -4,6 +4,7 @@ import module_cd from './cd.js'
 import module_echo from './echo.js'
 import module_ls from './ls.js'
 import module_mkdir from './mkdir.js'
+import module_mv from './mv.js'
 import module_putercmd from './putercmd.js'
 import module_rm from './rm.js'
 import module_tail from './tail.js'
@@ -14,6 +15,7 @@ export default {
     "echo": module_echo,
     "ls": module_ls,
     "mkdir": module_mkdir,
+    "mv": module_mv,
     "putercmd": module_putercmd,
     "rm": module_rm,
     "tail": module_tail,
---
+++ src/puter-shell/coreutils/mv.js
@@ -0,0 +1,75 @@
+import path from "path-browserify";
+
+export default {
+    name: 'mv',
+    args: {
+        $: 'simple-parser',
+        allowPositionals: true
+    },
+    execute: async ctx => {
+        const { positionals } = ctx.locals;
+        const { out, err, puterShell } = ctx.externs;
+
+        let isErr = false;
+
+        if ( positionals.length < 1 ) {
+            err.write('mv: missing file operand\n');
+            return;
+        }
+
+        const srcRelPath = positionals.shift();
+
+        if ( positionals.length < 1 ) {
+            const aft = positionals[0];
+            err.write(`mv: missing destination file operand after '${aft}'\n`);
+            return;
+        }
+
+        const dstRelPath = positionals.shift();
+
+        // DRY: also done in mkdir and cat
+        const resolve = relPath => {
+            if ( relPath.startsWith('/') ) {
+                return relPath;
+            }
+            return path.resolve(ctx.vars.pwd, relPath);
+        }
+
+        const srcAbsPath = resolve(srcRelPath);
+        let   dstAbsPath = resolve(dstRelPath);
+
+        // TODO: check if destination is a directory
+        const statOfDst = await puterShell.command(
+            'call-puter-api', {
+                command: 'stat',
+                params: { path: dstAbsPath }
+            }
+        );
+
+        let isDir = statOfDst.$ !== 'error' && statOfDst.is_dir;
+
+        if ( statOfDst.$ === 'error' ) {
+            if ( statOfDst.status === 404 ) console.log('YES IS 404');
+        }
+
+        console.log('dest is dir?', isDir);
+
+        let new_name = null;
+        if ( ! isDir ) {
+            new_name = path.basename(dstAbsPath);
+            dstAbsPath = path.dirname(dstAbsPath);
+        }
+
+        const result = await puterShell.command(
+            'call-puter-api', {
+                command: 'move',
+                params: {
+                    source_path: srcAbsPath,
+                    ...(new_name ? { new_name } : {}),
+                    dest_path: dstAbsPath,
+                    overwrite: true
+                }
+            }
+        );
+    }
+}
