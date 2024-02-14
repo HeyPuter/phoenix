fec3ae3c854d343b3fb6c3f112bb9b72527e1ea3
Sam Atkins <atkinssj@gmail.com>
2024-02-14 10:24:35 +0000

    Add false command

--- src/puter-shell/coreutils/__exports__.js
+++ src/puter-shell/coreutils/__exports__.js
@@ -25,6 +25,7 @@ import module_clear from './clear.js'
 import module_cp from './cp.js'
 import module_dcall from './dcall.js'
 import module_echo from './echo.js'
+import module_false from './false.js'
 import module_grep from './grep.js'
 import module_help from './help.js'
 import module_jq from './jq.js'
@@ -55,6 +56,7 @@ export default {
     "cp": module_cp,
     "dcall": module_dcall,
     "echo": module_echo,
+    "false": module_false,
     "grep": module_grep,
     "help": module_help,
     "jq": module_jq,
